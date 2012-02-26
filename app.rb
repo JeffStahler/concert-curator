406d1a9e8f105485b91619085ab31a0792c3d54d
Jeff Stahler <jeffreystahler@gmail.com>
2012-02-25 22:27:10 -0600

    added ability to search for events in a city between two dates

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ end
 
 
 post '/search' do
-  @metro_calendar = Songkick.get_metro_calendar(params[:city])
+  @metro_calendar = Songkick.get_events_by_city_and_dates(params[:city], params[:min_date], params[:max_date])
   erb :search
 end
 
@@ -13,19 +13,19 @@ end
 
 class Songkick
 	
-	API_KEY = 'UokXlLzqCN1zhOWe'
 	ROOT_URL = 'http://api.songkick.com/api/3.0/'
+	API_PARAM = 'apikey=UokXlLzqCN1zhOWe'
 	def self.get_loc_id(city)
 		city = URI.escape(city)
-		url = "#{ROOT_URL}search/locations.json?query=#{city}&apikey=#{API_KEY}" 
-		parsed_json_response = JSON.parse(RestClient.get(url))
-		loc_id = parsed_json_response['resultsPage']['results']['location'][0]['metroArea']['id']  
+		url = "#{ROOT_URL}search/locations.json?query=#{city}&#{API_PARAM}" 
+		json = JSON.parse(RestClient.get(url))
+		json['resultsPage']['results']['location'][0]['metroArea']['id']  
 	end
 
 	def self.get_metro_calendar_using_loc_id(loc_id)
-		url = "#{ROOT_URL}metro_areas/#{loc_id}/calendar.json?apikey=#{API_KEY}"  
-		parsed_json_response = JSON.parse(RestClient.get(url))
-		metro_calendar = parsed_json_response['resultsPage']['results']['event']  
+		url = "#{ROOT_URL}metro_areas/#{loc_id}/calendar.json?#{API_PARAM}"  
+		json = JSON.parse(RestClient.get(url))
+		json['resultsPage']['results']['event']  
 	end
 
 	def self.get_metro_calendar(city)
@@ -33,6 +33,10 @@ class Songkick
 		metro_calendar = self.get_metro_calendar_using_loc_id(loc_id)
 	end
 
-
-
+	def self.get_events_by_city_and_dates(city, min_date, max_date)
+		loc_id = self.get_loc_id(city)
+		url = "#{ROOT_URL}events.json?location=sk:#{loc_id}&min_date=#{min_date}&max_date=#{max_date}&#{API_PARAM}"
+		json = JSON.parse(RestClient.get(url))
+		json['resultsPage']['results']['event'] 
+	end 
 end 
