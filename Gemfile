9202e6d3c86054c56a24ef1f05f97f39283aca42
Jeff Stahler <jeffreystahler@gmail.com>
2012-02-25 20:21:37 -0600

    something

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'http://rubygems.org'
 gem 'sinatra'
 gem 'rest-client'
+gem 'shotgun'
+gem 'json'
--- hello.rb
+++ hello.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
-
+require 'rest_client'
+require 'json'
 get '/' do
-  "Hello World!"
+   response = RestClient.get 'http://ws.audioscrobbler.com/2.0/?method=venue.search&api_key=b25b959554ed76058ac220b7b2e0a026&venue=empty%20bottle&format=json'
+   response.to_s
 end
